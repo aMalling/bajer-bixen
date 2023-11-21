edfeb2983a50421a8e5225d5c88577aef34ca80d
Anders Malling privat <Andersmalling@gmail.com>
2023-11-21 22:17:00 +0100

    mobil adjustet navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function MyNavBar() {
   return (
-    <div className="grid grid-cols-3 shadow-md items-center bg-bajer-primary text-bajer-base">
+    <div className="flex justify-center  md:grid grid-cols-3 shadow-md items-center bg-bajer-primary text-bajer-base">
       <div className="flex pl-3 items-center justify-start">
         <Image
           src={require("../assets/bajerBiksen_logo.png")}
@@ -11,40 +11,12 @@ function MyNavBar() {
           width={50}
           alt="icon"
         />
-        <h2 className="text-xl ml-2 inline">BajerBiksen</h2>
+        <h2 className="hidden md:flex text-xl ml-2 inline">BajerBiksen</h2>
       </div>
 
       <h2 className="text-xl ml-2 font-semibold inline text-center">
         ØLJULEKALENDERN 2023
       </h2>
-
-      {/* <div className="flex flex-none justify-end col-start-2 col-span-1">
-        <Link
-          href="#julekalenderen"
-          className="p-1 ml-3 font-medium border-bajer-base px-2  hover:bg-bajer-secondary rounded-full"
-        >
-          JuleKalenderen
-        </Link>
-        <Link
-          href="#bottlesalg"
-          className="p-1 ml-3 font-medium border-bajer-base px-2  hover:bg-bajer-secondary rounded-full hidden md:flex "
-        >
-          Løssalg
-        </Link>
-        <Link
-          href="#beerontap"
-          className="p-1 ml-3 font-medium border-bajer-base px-2  hover:bg-bajer-secondary rounded-full hidden md:flex"
-        >
-          BeerOnTap
-        </Link>
-
-        <Link
-          href="#contact"
-          className="p-1 ml-3 font-medium border-bajer-base px-2  hover:bg-bajer-secondary rounded-full hidden md:flex"
-        >
-          Kontakt
-        </Link>
-      </div> */}
     </div>
   );
 }

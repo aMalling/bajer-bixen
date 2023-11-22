ddf729792de865170adaad6eb402223802e9d6c5
Anders Malling privat <Andersmalling@gmail.com>
2023-11-22 20:45:57 +0100

    add som padding rightlogo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function MyNavBar() {
   return (
-    <div className="flex justify-center  md:grid grid-cols-3 shadow-md items-center bg-bajer-primary text-bajer-base">
+    <div className="flex justify-center md:grid grid-cols-3 shadow-md items-center bg-bajer-primary text-bajer-base">
       <div className="flex pl-3 items-center justify-start">
         <Image
           src={require("../assets/bajerBiksen_logo.png")}
@@ -17,12 +17,14 @@ function MyNavBar() {
       <h2 className="text-xl ml-2 font-semibold inline text-center">
         ØLJULEKALENDERN 2023
       </h2>
-      <Image
-        src={require("../assets/bajerBiksen_logo.png")}
-        className=" md:hidden py-1 ml-2 inline bg-baj"
-        width={50}
-        alt="icon"
-      />
+      <div className="pr-3">
+        <Image
+          src={require("../assets/bajerBiksen_logo.png")}
+          className=" md:hidden py-1 ml-2 inline"
+          width={50}
+          alt="icon"
+        />
+      </div>
     </div>
   );
 }

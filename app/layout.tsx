eb0dd7c633517a6a859127ae64e4141935af09ba
Anders Malling privat <Andersmalling@gmail.com>
2023-11-27 20:46:23 +0100

    new titel

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "BajerBiksen",
+  title: "ØLJULEKALENDEREN 2023",
   description: "Byens Bedste Boomers",
 };
 
